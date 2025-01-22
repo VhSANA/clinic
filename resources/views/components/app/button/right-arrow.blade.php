fba73b8a43bb4e1a360b6542347be601191cb621
VhSANA <VhSANA@gmail.com>
2025-01-22 10:51:10 +0330

    some icon components created to be shown in calendar

---
+++ resources/views/components/app/button/right-arrow.blade.php
@@ -0,0 +1,5 @@
+<button
+class="flex justify-between items-center gap-2 text-white bg-blue-500 hover:bg-blue-600 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5  mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 transition">
+    <x-icons.right-arrow />
+    <p>{{$slot}}</p>
+</button>
