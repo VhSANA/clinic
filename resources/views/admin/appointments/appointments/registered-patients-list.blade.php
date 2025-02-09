d91b4137a8763a5c9575a6503d23cf1f2bdeca92
VhSANA <VhSANA@gmail.com>
2025-02-09 15:57:03 +0330

    earase search box added to view

--- resources/views/admin/appointments/appointments/registered-patients-list.blade.php
+++ resources/views/admin/appointments/appointments/registered-patients-list.blade.php
@@ -12,6 +12,7 @@
                                             <svg class="w-5 h-5 text-gray-500 dark:text-gray-400" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
                                         </div>
                                         <input type="text" id="table-search" class="block p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="جستجو">
+                                        <button type="button" class="hidden absolute left-2 top-2">❌</button>
                                     </div>
                                 </div>
                                 <div>
@@ -51,7 +52,7 @@
                                                 <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
                                                     <th scope="row" class="flex items-center justify-center px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                                                         <div class="ps-3">
-                                                            <div class="text-base font-semibold">{{ $appointment->patient->full_name }}</div>
+                                                            <div class="text-base font-semibold px-6 py-4">{{ $appointment->patient->full_name }}</div>
                                                         </div>
                                                     </th>
                                                     <td class="px-6 py-4">
@@ -106,6 +107,7 @@
             // value of inputs
             const search = document.getElementById('table-search');
             const patientsTable = document.getElementById('patients-table');
+            const eraseBtn = search.nextElementSibling;
 
             // search patient
             search.addEventListener('change', function (e) {
@@ -116,6 +118,9 @@
 
                 // Clear existing rows
                 patientsTable.innerHTML = '';
+
+                // display erase button
+                eraseBtn.classList.remove('hidden');
 
                 // Render filtered patients
                 if (filteredPatients.length > 0) {
@@ -167,6 +172,25 @@
                     `;
                     patientsTable.appendChild(row);
                 }
+
+                if (searchedValue == '') {
+                    patientsTable.innerHTML = `
+                        <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                            <td colspan="8" class="flex justify-center items-center px-6 py-4">کد ملی یا نام بیمار راجستجو کنید <x-icons.search /></td>
+                        </tr>
+                    `;
+                }
+            });
+
+            // Erase button functionality
+            eraseBtn.addEventListener('click', function () {
+                search.value = '';
+                patientsTable.innerHTML = `
+                    <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                        <td colspan="8" class="flex justify-center items-center px-6 py-4">کد ملی یا نام بیمار راجستجو کنید <x-icons.search /></td>
+                    </tr>
+                `;
+                eraseBtn.classList.add('hidden');
             });
 
         // functions
